d492244b3f2f5ee9702457862326c548e5091e2a
Syahrial Agni Prasetya <syahrial@lemniskett.moe>
2024-07-18 12:03:22 +0700

    Add wsgi_prefork_http11

---
+++ wsgi_prefork_http11/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'wsgi_prefork_http11',
+    'summary': 'Enables HTTP/1.1 support on WSGI prefork mode',
+    'version': '16.0',
+    'website': 'https://github.com/mplus-oss/cloud-addons',
+    'author': 'M+ Software',
+    'license': 'LGPL-3',
+    'category': 'Tools',
+    'depends': ['web'],
+    'auto_install': False,
+    'installable': False,
+}
